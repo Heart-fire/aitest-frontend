c6bf6b621f254bccddd3d481006a2ee5601aa83c
Heart-fire <wangchunbo384@gmail.com>
2024-08-30 15:19:13 +0800

    @Heart-fire@前端项目初始化并引入组件库

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="basicLayout">
+    <!-- vh:不管屏幕有多高,总是占屏幕的100% -->
+    <a-layout style="height: 100vh">
+      <a-layout-header class="Header">
+        <GlobalHeader />
+      </a-layout-header>
+      <!-- 中间的内容需要复用这个布局,需要动态替换的-->
+      <a-layout-content class="Content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://www.code-nav.cn" target="_blank">脚部</a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
+<!--为了防止冲突，先找basicLayout下面的ID-->
+<style scoped>
+#basicLayout .Content {
+  background: linear-gradient(to right, #fefefe, #fff);
+  margin-bottom: 28px;
+  padding: 20px;
+}
+#basicLayout .footer {
+  background: #f5e1e1;
+  padding: 20px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+// 项目的全局入口文件
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import ArcoVue from "@arco-design/web-vue";
+import "@arco-design/web-vue/dist/arco.css";
+
+createApp(App).use(ArcoVue).use(router).mount("#app");
